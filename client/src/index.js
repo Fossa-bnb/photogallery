8befcfbc267d08ff607aa09b0b7ed7117f4f2b70
David Seidenberg <davidseidenberg@gmail.com>
2018-02-23 17:06:18 -0800

    Create PhotoGallery stateful component and render to DOM

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My Minimal React Webpack Babel Setup';
+
+class PhotoGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+   }
+   
+   render() {
+    return (
+      <div className="photo-gallery">
+        Photo Gallery Module
+      </div>
+    )
+   }
+}
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <PhotoGallery />,
   document.getElementById('app')
 );
