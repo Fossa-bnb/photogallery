881d2830c9b798a3eec75da945720e9f29a0363a
David Seidenberg <davidseidenberg@gmail.com>
2018-02-23 17:23:31 -0800

    Pass in id via req.params to server

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ app.use('/', (req, res, next) => {
 app.use(express.static(path.resolve(__dirname, '../dist')));
 
 app.get('/:id', (req, res) => {
+  console.log('ahoy' + req.params.id);
   res.send('received get request from component');
+  // 
 });
 
 app.listen(3000, () => {
