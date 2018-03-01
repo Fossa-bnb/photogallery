b32e45771ed63d6fa04af649f03ee8a4243e983b
David Seidenberg <davidseidenberg@gmail.com>
2018-03-01 10:37:11 -0700

    Fix arrow styling

--- client/src/Spotlight.js
+++ client/src/Spotlight.js
@@ -5,9 +5,8 @@ const Spotlight = (props) => {
     <div className="spotlight">
       <img className="spotlight-img" src={props.photo.url} alt={props.photo.caption} />
       <div className="spotlight-info">
-        <div className="numbertext">{props.photo.photoId} / {props.numPhotos}</div>
         <div className="caption-container">
-          <p id="caption">&nbsp;&nbsp;{props.photo.caption}</p>
+          <p id="caption">{props.photo.photoId} / {props.numPhotos} &nbsp;&nbsp;{props.photo.caption}</p>
         </div>
       </div>
     </div>
