11bd7d929b2529eb66649abd7c980c7d262e4404
David Seidenberg <davidseidenberg@gmail.com>
2018-02-28 21:40:39 -0700

    Refactor - Style gallery as lightbox

--- client/src/Thumbnail.js
+++ client/src/Thumbnail.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Thumbnail = (props) => {
   return (
-    <div className="thumbnail">
-      <img src={props.photo.url} />
+    <div className="thumbnail-container">
+      <div className="thumbnail">
+        <img className="thumbnail-img" src={props.photo.url} />
+      </div>
     </div>
   )
 }
--- client/src/ThumbnailList.js
+++ client/src/ThumbnailList.js
@@ -4,7 +4,6 @@ import Thumbnail from './Thumbnail';
 const ThumbnailList = (props) => {
   return (
     <div className="thumbnail-list">
-      Thumbnail List
       {props.photos.map((photo, index) => {
         return <Thumbnail key={index} photo={photo} />
       })}
