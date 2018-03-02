497d197c7f46421decb6df359fea24cd913e0fc1
David Seidenberg <davidseidenberg@gmail.com>
2018-03-02 14:04:37 -0700

    Hover toggles thumbnails, need to add click functionality

--- client/src/Spotlight.js
+++ client/src/Spotlight.js
@@ -8,11 +8,11 @@ const Spotlight = ({photo, numPhotos, plusSlides, toggleThumbnails}) => {
         <div className="caption-container">
           <p id="caption">{photo.photoId} / {numPhotos}: &nbsp;{photo.caption}</p>
         </div>
-        <div className="toggle-thumbnails">
-          <div className="hidden-thumbnails" onMouseOver={toggleThumbnails}>
+        <div className="toggle-thumbnails" onMouseEnter={() => {toggleThumbnails(true)}}>
+          <div className="hidden-thumbnails">
             <p>Show Photo List</p>
           </div>
-          <div className="shown-thumbnails" onClick={toggleThumbnails} >
+          <div className="shown-thumbnails" onClick={() => {toggleThumbnails(false)}} >
             <p>Hide Photo List</p>
           </div>
         </div>
