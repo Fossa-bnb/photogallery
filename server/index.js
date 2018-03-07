cd750c2d5ff8bb2cf6311b441498dd849fb9dc86
David Seidenberg <davidseidenberg@gmail.com>
2018-03-07 11:27:49 -0800

    Refactor photoGallery to work on proxy server and on port 3000 as solo component simultaneously

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use('/', (req, res, next) => {
   next();
 });
 
-app.use(express.static(path.resolve(__dirname, '../')));
+app.use('/', express.static(path.resolve(__dirname, '../')));
 
 app.get('/:id', (req, res) => {
   retrievePhotos(req.params.id, (err, photos) => {
