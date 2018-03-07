d5df401c4e111f253ce85587b5ead861b2c4146b
David Seidenberg <davidseidenberg@gmail.com>
2018-03-06 16:30:18 -0800

    Refactored banner to use CSS modules

--- client/src/Banner.js
+++ client/src/Banner.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './styles.css';
 
 const Banner = ({photo1URL, photo2URL, toggleGalleryView}) => {
   return (
-    <div className="banner" onClick={toggleGalleryView}>
-      <img className="left-img img-responsive"  src={photo1URL}/>
-      <img className="right-img img-responsive" src={photo2URL}/>
+    <div className={ styles.banner } onClick={toggleGalleryView}>
+      <img className={ styles.left_img }  src={photo1URL}/>
+      <img className={ styles.right_img } src={photo2URL}/>
       <button>View Photos</button>
     </div>
   )
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Photos from './Photos';
-import styles from './app.css';
+import styles from './styles.css';
+
+console.log(styles);
 
 ReactDOM.render(
   <Photos myId={2} />,
